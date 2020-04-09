72da5fab31d421f047471e98202de5d1b998e0a0
hayabusa09060 <hayabusableach@gmail.com>
2020-04-09 19:49:28 +0900

    床仮途中経過

---
+++ Sound/Assets/ooishi/Scripts/Block.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum BlockType
+{
+    Nomal,
+    Fire
+}
+public class Block : MonoBehaviour
+{
+    public BlockType block;
+    public int hp;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Move()
+    {
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.transform.tag=="Player")
+        {
+            hp--;
+            if(hp<0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}

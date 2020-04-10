033b27d50a274db96b497911e206934bbd173f91
hayabusa09060 <hayabusableach@gmail.com>
2020-04-10 15:18:42 +0900

    隣接ブロック実装したぜー ごりらー

--- Sound/Assets/ooishi/Scripts/Block.cs
+++ Sound/Assets/ooishi/Scripts/Block.cs
@@ -10,31 +10,107 @@ public class Block : MonoBehaviour
 {
     public BlockType block;
     public int hp;
+    private int sethp;
     public float speed;
+    private bool hitflag;
+    private Renderer color,savecolor;
+    private Dictionary<int,Block> blocks;
+    private Dictionary<int,LinkBlock> linkBlocks;
+    private int count;
+    public bool damageflag;
+    private Player player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        blocks = new Dictionary<int, Block>();
+        linkBlocks = new Dictionary<int, LinkBlock>();
+        color =gameObject.transform.GetComponent<Renderer>();
+        savecolor = color;
+        if(block==BlockType.Nomal)
+        {
+            sethp = 1;
+            if(hp<=sethp)
+            {
+                hp = sethp;
+            }
+        }else
+        {
+            sethp = 2;
+            if (hp <= sethp)
+            {
+                hp = sethp;
+            }
+        }
+        count = 0;
+        foreach(Transform child in transform)
+        {
+            linkBlocks.Add(count,child.gameObject.GetComponent<LinkBlock>()) ;
+            blocks.Add(count, gameObject.GetComponent<Block>());
+            count++;
+        }
+        player = GameObject.Find("Player").gameObject.GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i=0;i<linkBlocks.Count-1;i++)
+        {
+            if(linkBlocks[i].hitflag)
+            {
+                blocks[i]=linkBlocks[i].hitblock;
+            }
+        }
+        Damage();
     }
     void Move()
     {
 
     }
+
+    void Damage()
+    {
+        for(int i=0;i<blocks.Count-1;i++)
+        {
+            if(blocks[i].damageflag&&!damageflag)
+            {
+                damageflag = true;
+                color.material.color = Color.red;
+            }
+        }
+        if ((block == BlockType.Nomal &&player.type == PlayerType.Fire)
+            || (block == BlockType.Fire && player.type == PlayerType.Nomal))
+        {
+            if (damageflag)
+            {
+                hp--;
+                damageflag = false;
+                color.material.color = Color.white;
+            }
+        }
+        if (hp < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            hitflag = true;
+            color.material.color = Color.red;
+        }
+    }
+
     private void OnCollisionExit(Collision collision)
     {
         if(collision.transform.tag=="Player")
         {
-            hp--;
-            if(hp<0)
-            {
-                Destroy(gameObject);
-            }
+
+                damageflag = true;
+                hitflag = false;
+               // color.material.color = Color.white;
         }
     }
 }
---
+++ Sound/Assets/ooishi/Scripts/LinkBlock.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkBlock : MonoBehaviour
+{
+    public bool hitflag;
+    public Block hitblock;
+    private Block block,save;
+    private Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        block = transform.root.gameObject.transform.gameObject. GetComponent<Block>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag=="Block")
+        {
+            save = other.transform.gameObject.GetComponent<Block>();
+            if(block.block==save.block)
+            {
+                other.transform.gameObject.GetComponent<Renderer>().material.color = Color.black;
+                hitflag = true;
+                hitblock = other.transform.GetComponent<Block>();
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Block")
+        {
+            hitflag = false;
+        }
+    }
+}

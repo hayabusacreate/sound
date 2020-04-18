7bf1cba83e9ba4c4be36e017ae814ac5e557268c
Imamura728 <image50804650@gmail.com>
2020-04-18 20:51:07 +0900

    半透明シェーダー、床、パーティクルテスト

---
+++ Sound/Assets/imamura/CheckObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckObject : MonoBehaviour
+{
+    
+
+    
+    Camera mainCamera;
+    
+    GameObject gameObj;
+
+
+    bool a = false;
+
+    void Update()
+    {
+     
+        Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2 -5);
+        Ray ray = Camera.main.ScreenPointToRay(center);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            if(hit.collider.tag == "Block")
+            {
+                gameObj = hit.collider.gameObject;
+
+                gameObj.GetComponent<ChangeMat>().CMat();
+            }
+        }
+        else
+        {
+            
+        }
+
+
+
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.red, 5);
+
+    }
+
+    
+}

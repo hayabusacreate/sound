6ffe2c7cf07b07fae1ac5d136a1e1a1849f6fe63
hayabusa09060 <hayabusableach@gmail.com>
2020-04-20 20:16:31 +0900

    wave

---
+++ Sound/Assets/ooishi/Scripts/Wave.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    public GameObject[] wave;
+    public int[] tyle;
+    public int wavetime;
+    private float time;
+    private int count;
+    private MapCreate map;
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = GameObject.Find("MapCreate").GetComponent<MapCreate>();
+        count = 0;
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time>wavetime&&wave.Length>count)
+        {
+            GameObject gameObject= Instantiate(wave[count], transform.position, Quaternion.identity);
+            gameObject.transform.rotation = Quaternion.Euler(-90,0, (360 / map.inblock) * tyle[count]);
+            count++;
+            time = 0;
+        }
+    }
+}

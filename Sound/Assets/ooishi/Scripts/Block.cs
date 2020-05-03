c5ad4c2ad16f3c1b39fefc5c44ebacf0583c0903
hayabusa09060 <hayabusableach@gmail.com>
2020-05-03 13:45:08 +0900

    konnnitiha

--- Sound/Assets/ooishi/Scripts/Block.cs
+++ Sound/Assets/ooishi/Scripts/Block.cs
@@ -56,9 +56,11 @@ public class Block : MonoBehaviour
     public Rigidbody rigidbody;
 
     private Manager manager;
+    //private Material mat;
     // Start is called before the first frame update
     void Start()
     {
+        //mat.color = new Color(mat.color.r, mat.color.g, mat.color.b,0);
         manager = GameObject.Find("Manager").gameObject.GetComponent<Manager>();
         quaternion = this.transform.rotation;
         savequaternion = this.transform.rotation;

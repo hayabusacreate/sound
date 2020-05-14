bfe30f34c13c1eff0072b08887fafc4f367f743b
Imamura728 <image50804650@gmail.com>
2020-05-14 14:27:38 +0900

    yukaFireパーティクルが広がりながら出てくるように調整

---
+++ Sound/Assets/imamura/Scripts/particleScale.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleScale : MonoBehaviour
+{
+    ParticleSystem m_particle;
+    float scale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale = 0;
+        m_particle = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    scale += 0.1f;
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    scale -= 0.1f;
+        //}
+
+        //if(Input.GetKeyDown(KeyCode.A))
+        //{
+        //    scale = 0;
+        //}
+
+        if(scale <= 1 && m_particle.isPlaying)
+        {
+            scale = scale + Time.deltaTime + Time.deltaTime;
+        }
+        
+
+        var sh = m_particle.shape;
+
+        sh.scale = new Vector3(scale, 1, 0);
+    }
+}

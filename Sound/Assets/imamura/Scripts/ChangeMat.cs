afe43cb4a7eca0a4fef969382d394bbedcad831e
Imamura728 <image50804650@gmail.com>
2020-05-21 21:16:18 +0900

    地面が真っ黒になるバグを修正・真

--- Sound/Assets/imamura/Scripts/ChangeMat.cs
+++ Sound/Assets/imamura/Scripts/ChangeMat.cs
@@ -20,7 +20,7 @@ public class ChangeMat : MonoBehaviour
         
 
         //transform.GetComponent<Renderer>().material.SetFloat("_Threshold",1.0f - (block.hp / block.Maxhp) - 0.7f);
-        transform.GetComponent<Renderer>().material.SetColor("_Color", new Color(1.0f - (block.hp / block.Maxhp) + 0.25f, 0,0,0));
+        transform.GetComponent<Renderer>().material.SetColor("_Color", new Color(1.0f, (block.hp / block.Maxhp), (block.hp / block.Maxhp), 1));
     }
 
     

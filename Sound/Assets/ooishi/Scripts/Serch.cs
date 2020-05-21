8161350370888f671f8e01269b2494a1629c38e0
hayabusa09060 <hayabusableach@gmail.com>
2020-05-21 14:57:11 +0900

    エネミー生成

--- Sound/Assets/ooishi/Scripts/Serch.cs
+++ Sound/Assets/ooishi/Scripts/Serch.cs
@@ -16,7 +16,7 @@ public class Serch : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.tag=="Player")
         {

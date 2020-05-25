6b77ef0932731c16baca5b7393a4d73c5120e69f
Imamura728 <image50804650@gmail.com>
2020-05-25 19:06:02 +0900

    tanaka2にマージ

---
+++ Sound/Assets/ooishi/Scripts/ChangeCam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class ChangeCam : MonoBehaviour
+{
+    public CinemachineVirtualCamera start, end;
+    public GameObject endobj;
+    public bool changeflag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(changeflag)
+        {
+            end.Priority = 100;
+            start.Priority = 0;
+        }
+    }
+}

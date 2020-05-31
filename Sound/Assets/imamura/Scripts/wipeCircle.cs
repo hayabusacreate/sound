14553eec4caaf5432316de7a90ecc218a8dc7de8
Imamura728 <image50804650@gmail.com>
2020-05-31 19:49:59 +0900

    ワイプをlerpと中点を通るように修正

--- Sound/Assets/imamura/Scripts/wipeCircle.cs
+++ Sound/Assets/imamura/Scripts/wipeCircle.cs
@@ -9,6 +9,10 @@ public class wipeCircle : MonoBehaviour
 
     float value;
 
+    public float point1, point2,speed1,speed2, speed3;
+
+    bool one, two;
+
     void Start()
     {
         if(gameObject.name == "Open")
@@ -21,28 +25,67 @@ public class wipeCircle : MonoBehaviour
             value = 2;
             wipeMat.SetFloat("_Radius", 2);
         }
+
+        one = false;
+        two = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKey(KeyCode.Space))
-        //{
-        //    value = value + Time.deltaTime;
-        //    wipeMat.SetFloat("_Radius", value);
-        //    Debug.Log(wipeMat);
-        //}
-        if (gameObject.name == "Open" && value <= 2)
+        
+        if (gameObject.name == "Open")
         {
-            value = value + Time.deltaTime*2;
-            wipeMat.SetFloat("_Radius", value);
-            Debug.Log(wipeMat);
+            if(value <= point1 && one == false)
+            {
+                value = Mathf.Lerp(value, point1, Time.deltaTime * speed1);
+                wipeMat.SetFloat("_Radius", value);
+                if(value >= (point1 - 0.1f))
+                {
+                    one = true;
+                }
+            }
+            else if(one == true && two == false)
+            {
+                value = Mathf.Lerp(value, point2, Time.deltaTime * speed2);
+                wipeMat.SetFloat("_Radius", value);
+                if (value <= (point2 + 0.1f))
+                {
+                    two = true;
+                }
+            }
+            else if (value <= 2 && two == true && one == true)
+            {
+                value = Mathf.Lerp(value, 2, Time.deltaTime * speed3);
+                wipeMat.SetFloat("_Radius", value);
+            }
         }
-        else if (gameObject.name == "Close" && value >= 0)
+        else if (gameObject.name == "Close")
         {
-            value = value - Time.deltaTime*2;
-            wipeMat.SetFloat("_Radius", value);
-            Debug.Log(wipeMat);
+            if(value >= point1 && one == false)
+            {
+                value = Mathf.Lerp(value, point1, Time.deltaTime * speed1);
+                wipeMat.SetFloat("_Radius", value);
+                if(value <= (point1 + 0.1f))
+                {
+                    one = true;
+                }
+            }
+            else if(one == true && two == false)
+            {
+                Debug.Log(value);
+                value = Mathf.Lerp(value, point2, Time.deltaTime * speed2);
+                wipeMat.SetFloat("_Radius", value);
+                if (value >= (point2 - 0.1f))
+                {
+                    two = true;
+                }
+            }
+            else if (value <= 2 && two == true && one == true)
+            {
+                value = Mathf.Lerp(value, 0, Time.deltaTime * speed3);
+                wipeMat.SetFloat("_Radius", value);
+            }
         }
 
     }

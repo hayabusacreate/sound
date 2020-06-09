a9c20cc81d1e75458721a21b94d2d0772dac91a4
Imamura728 <image50804650@gmail.com>
2020-06-09 16:15:03 +0900

    ゲージが溜まる演出を追加、ステセレを少し追加

--- Sound/Assets/imamura/Scripts/getFill.cs
+++ Sound/Assets/imamura/Scripts/getFill.cs
@@ -7,17 +7,41 @@ public class getFill : MonoBehaviour
 {
 
     public Image cooldown;
+
+    public SceneChange sc;
+
+    public float value;
+    public float a = 0;
+
+    public float speed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        cooldown = gameObject.GetComponent<Image>();
+        sc = GameObject.Find("SceneChange").GetComponent<SceneChange>();
+        cooldown.material.SetFloat("_Ratio", 0);
+        value= 0;
+        a = 0;
+}
 
     // Update is called once per frame
     void Update()
     {
-        
+        a = (Mathf.Floor(value *100)/100) +(1 / (float)sc.mapCreate.maxblock);
         cooldown = gameObject.GetComponent<Image>();
-        cooldown.material.SetFloat("_Ratio", cooldown.fillAmount);
+        if (value < cooldown.fillAmount && sc.mapCreate.maxblock - sc.mapCreate.blocks != 0)
+        {
+            value += Time.deltaTime * speed;
+        }
+
+        //a = cooldown.fillAmount;
+
+        //if(Input.GetKey(KeyCode.J))
+        //{
+        //    value += Time.deltaTime;
+        //    Debug.Log("dada");
+        //}
+        cooldown.material.SetFloat("_Ratio", Mathf.Floor(value*100)/100);
     }
 }

e7c91b463d103ca14ad6dafdb7360531e01bb7b1
Imamura728 <image50804650@gmail.com>
2020-06-09 16:32:46 +0900

    火の粉調整したよ

--- Sound/Assets/imamura/Scripts/HinokoPos.cs
+++ Sound/Assets/imamura/Scripts/HinokoPos.cs
@@ -16,6 +16,6 @@ public class HinokoPos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = camera.transform.position + new Vector3(0, 5, -10);
+        transform.position = camera.transform.position + new Vector3(0, 3.5f, -10);
     }  
 }

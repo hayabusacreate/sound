dfb2fb31d52f624e03365b34ff05ed4c48d9c2a7
Imamura728 <image50804650@gmail.com>
2020-06-11 11:55:25 +0900

    床が燃える演出めっちゃ凝った

---
+++ Sound/Assets/imamura/Scripts/particleScaleBLR.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleScaleBLR : MonoBehaviour
+{
+    float scale;
+    // Start is called before the first frame update
+    public float speed = 1;
+
+    ParticleSystem m_particle;
+
+    float value;
+
+    Vector3 pos;
+
+    bool done;
+
+    GameObject par;
+
+    void Start()
+    {
+        scale = 0;
+        speed = 1;
+        m_particle = transform.GetComponent<ParticleSystem>();
+        pos = transform.position;
+        par = transform.root.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (scale <= 1 && m_particle.isPlaying)
+            {
+                scale = scale + Time.deltaTime + Time.deltaTime;
+            }
+        if (par.name == "YukaFireTop")
+        {
+            if (done == false)
+            {
+                if (transform.position.x >= pos.x - 0.45f && m_particle.isPlaying && transform.name == "fireBotR")
+                {
+                    value -= Time.deltaTime;
+
+                }
+                else if (value <= -0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(pos.x + value, transform.position.y, transform.position.z);
+            }
+
+            if (done == false)
+            {
+                if (transform.position.x <= pos.x + 0.45f && m_particle.isPlaying && transform.name == "fireBotL")
+                {
+                    value += Time.deltaTime;
+                }
+                else if (value >= 0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(pos.x + value, transform.position.y, transform.position.z);
+            }
+        }
+        else if (par.name == "YukaFireRight")
+        {
+            if (done == false)
+            {
+                if (transform.position.y >= pos.y - 0.45f && m_particle.isPlaying && transform.name == "fireLeftT")
+                {
+                    value -= Time.deltaTime;
+
+                }
+                else if (value <= -0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(transform.position.x, pos.y + value, transform.position.z);
+            }
+
+            if (done == false)
+            {
+                if (transform.position.y <= pos.y + 0.45f && m_particle.isPlaying && transform.name == "fireLeftB")
+                {
+                    value += Time.deltaTime;
+                }
+                else if (value >= 0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(transform.position.x, pos.y + value, transform.position.z);
+            }
+        }
+        else if (par.name == "YukaFireLeft")
+        {
+            if (done == false)
+            {
+                if (transform.position.y >= pos.y - 0.45f && m_particle.isPlaying && transform.name == "fireRightT")
+                {
+                    value -= Time.deltaTime;
+
+                }
+                else if (value <= -0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(transform.position.x, pos.y + value, transform.position.z);
+            }
+
+            if (done == false)
+            {
+                if (transform.position.y <= pos.y + 0.45f && m_particle.isPlaying && transform.name == "fireRightB")
+                {
+                    value += Time.deltaTime;
+                }
+                else if (value >= 0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(transform.position.x, pos.y + value, transform.position.z);
+            }
+        }
+        else if (par.name == "YukaFireBot")
+        {
+            if (done == false)
+            {
+                if (transform.position.x >= pos.x - 0.45f && m_particle.isPlaying && transform.name == "fireTopR")
+                {
+                    value -= Time.deltaTime;
+
+                }
+                else if (value <= -0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(pos.x + value, transform.position.y, transform.position.z);
+            }
+
+            if (done == false)
+            {
+                if (transform.position.x <= pos.x + 0.45f && m_particle.isPlaying && transform.name == "fireTopL")
+                {
+                    value += Time.deltaTime;
+                }
+                else if (value >= 0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(pos.x + value, transform.position.y, transform.position.z);
+            }
+        }
+
+
+        var main = m_particle.main;
+
+        var sh = m_particle.shape;
+        
+        main.startSpeed = speed;
+
+        sh.scale = new Vector3(0.5f, scale, 0);
+
+        if (transform.name == "fireBotR" && done == true)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.name == "fireLeftB" && done == true)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.name == "fireRightB" && done == true)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.name == "fireTopR" && done == true)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Sound/Assets/imamura/Scripts/particleScaleLR.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleScaleLR : MonoBehaviour
+{
+    float scale;
+    // Start is called before the first frame update
+    public float speed = 1;
+
+    ParticleSystem m_particle;
+
+    [SerializeField]
+    GameObject botLeft;
+
+    [SerializeField]
+    GameObject botRight;
+
+    float value;
+
+    Vector3 pos;
+
+    bool done;
+
+    GameObject par;
+
+    void Start()
+    {
+        scale = 0;
+        speed = 1;
+        m_particle = transform.GetComponent<ParticleSystem>();
+        pos = transform.position;
+        par = transform.root.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            if (scale <= 1 && m_particle.isPlaying)
+            {
+                scale = scale + Time.deltaTime + Time.deltaTime;
+            }
+            else if (scale >= 1)
+            {
+            botLeft.SetActive(true);
+            if (botRight != null)
+            {
+                botRight.SetActive(true);
+            }
+
+        }
+        if (par.name == "YukaFireTop")
+        {
+            if (done == false)
+            {
+                if (transform.position.y >= pos.y - 0.45f && m_particle.isPlaying)
+                {
+                    value -= Time.deltaTime;
+
+                }
+                else if (value <= -0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(transform.position.x, pos.y + value, transform.position.z);
+            }
+        }
+        else if (par.name == "YukaFireRight")
+        {
+            if (done == false)
+            {
+                if (transform.position.x >= pos.x - 0.45f && m_particle.isPlaying)
+                {
+                    value -= Time.deltaTime;
+
+                }
+                else if (value <= -0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(pos.x + value, transform.position.y, transform.position.z);
+            }
+        }
+        else if (par.name == "YukaFireLeft")
+        {
+            if (done == false)
+            {
+                if (transform.position.x <= pos.x + 0.45f && m_particle.isPlaying)
+                {
+                    value += Time.deltaTime;
+                }
+                else if (value >= 0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(pos.x + value, transform.position.y, transform.position.z);
+            }
+        }
+        else if (par.name == "YukaFireBot")
+        {
+            if (done == false)
+            {
+                if (transform.position.y <= pos.y + 0.45f && m_particle.isPlaying)
+                {
+                    value += Time.deltaTime;
+
+                }
+                else if (value >= 0.45f)
+                {
+                    done = true;
+                }
+                transform.position = new Vector3(transform.position.x, pos.y + value, transform.position.z);
+            }
+        }
+        var main = m_particle.main;
+
+        var sh = m_particle.shape;
+        
+        main.startSpeed = speed;
+
+        sh.scale = new Vector3(0.5f, scale, 0);
+
+        
+        
+    }
+}

a0cc2c0248cbe46bc7eb236aeed873d8b255abf0
hayabusa09060 <hayabusableach@gmail.com>
2020-06-11 11:22:46 +0900

    死亡アニメーション

--- Sound/Assets/ooishi/Scripts/Block.cs
+++ Sound/Assets/ooishi/Scripts/Block.cs
@@ -261,6 +261,7 @@ public class Block : MonoBehaviour
             if(block==BlockType.Goal)
             {
                 sceneChange.creaflag = true;
+               player.anim.SetTrigger("Death");
             }
             savecount = player.movecount;
             //color.material.color = Color.red;

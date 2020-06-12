12913b9654beae1328353f29a1cfdbee211df02c
Imamura728 <image50804650@gmail.com>
2020-06-12 12:09:24 +0900

    花火を出す処理と花火パーティクルを追加

---
+++ Sound/Assets/imamura/Scripts/hanabiEmit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hanabiEmit : MonoBehaviour
+{
+    [SerializeField]
+    GameObject hanabi;
+
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(hanabi);
+        Instantiate(hanabi);
+        Instantiate(hanabi);
+        Instantiate(hanabi);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= 2)
+        {
+            Instantiate(hanabi);
+            time = 0;
+        }
+    }
+}

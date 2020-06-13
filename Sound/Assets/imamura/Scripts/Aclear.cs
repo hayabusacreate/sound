ebb9b3575a67a1775ba76b093708982d84a86b2d
Imamura728 <image50804650@gmail.com>
2020-06-14 00:46:06 +0900

    ゲーミングマテリアルをゴミ箱からサルベージ、オールクリア後の演出を軽く実装

---
+++ Sound/Assets/imamura/Scripts/Aclear.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aclear : MonoBehaviour
+{
+    [SerializeField]
+    GameObject shell;
+
+    [SerializeField]
+    GameObject slime;
+
+    [SerializeField]
+    Material gShell;
+
+    [SerializeField]
+    Material gSlime;
+
+    [SerializeField]
+    GameObject sc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(sc.GetComponent<SceneChange>().allflag == true)
+        {
+            shell.GetComponent<Renderer>().material = gShell;
+            slime.GetComponent<Renderer>().material = gSlime;
+        }
+    }
+}
